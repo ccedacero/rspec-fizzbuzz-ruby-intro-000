6ffb50574b6587756a0017ea4721b79dcd2c1638
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-16 15:00:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,8 +5,10 @@ def fizzbuzz(int)
     puts "Buzz"
   elseif (int % 3 == 0 && int % 5 == 0) 
     puts "fizzbuzz"
-  end
+  else 
+  return nil 
 end
+end 
 
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
