9a637cfb7c0ea6ea9d1769382ac59be5810866ed
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-16 15:14:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 
-    return "fizzbuzz"
+    return "Fizzbuzz"
   elsif
     int % 3 == 0 # if the number int is divisible by 3
     return "Fizz" # Go fizz
