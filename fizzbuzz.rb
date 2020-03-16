fb55397e23559bbd646ade2aa307210e82057858
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-16 14:58:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,9 +7,4 @@ def fizzbuzz(int)
     puts "fizzbuzz"
   end
 end
-fizzbuzz(3) # => You should see a return of "Fizz"
-fizzbuzz(5) # => You should see a return of nil
-fizzbuzz()  # => You should get an ArgumentError
-end
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+
