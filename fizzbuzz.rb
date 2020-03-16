2943c8e6f19165aca13906c8ace8e97c0c2e9739
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-16 15:03:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if (int % 3 == 0) # if the number int is divisible by 3
+  if int % 3 == 0 # if the number int is divisible by 3
   print "Fizz" # Go fizz
-  elsif (int % 5 == 0)
+  elsif int % 5 == 0
     print "Buzz"
-  elsif (int % 3 == 0 && int % 5 == 0) 
+  elsif int % 3 == 0 && int % 5 == 0 
     print "fizzbuzz"
 end
 return nil
